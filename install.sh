c97a40c7df48737b5dcfeb1247841b00a25748ac
Kyliekyler <siosonkylebrandon@gmail.com>
2022-07-04 00:57:58 +0800

    updater: update to v2
    Signed-off-by: Kyliekyler <siosonkylebrandon@gmail.com>

--- install.sh
+++ install.sh
@@ -1,29 +1,42 @@
 print_modname() {
   $BB sleep 1
   echo " "
-  echo "================================================"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-  echo "   _____ __________ _____ ____ _____ _________  "
-  echo "  |     | _  |   __|   | |  __|_   _| _  | __ | "
-  echo "  | | | |    |  |  | | | |  __| | | |    |   -| "
-  echo "  |_|_|_|_|__|_____|_|___|____| |_| |_|__|_|__| "
-  echo "                                  by Kyliekyler "
-  echo "================================================"
+  echo "======================================================"
+  echo "     _____ __________ _____ ____ _____ _________      "
+  echo "    |     | _  |   __|   | |  __|_   _| _  | __ |     "
+  echo "    | | | |    |  |  | | | |  __| | | |    |   -|     "
+  echo "    |_|_|_|_|__|_____|_|___|____| |_| |_|__|_|__|     "
+  echo "                                    by Kyliekyler     "
+  echo "======================================================"
 }
 
 on_install() {
   echo " "
-  echo "- DOWNLOAD LATEST MAGNETAR RELEASE"
-  echo " "
-  echo "- OPENING PLING..."
-  $BB sleep 3
-  [ -n "$($BB which xdg-open)" ] && $BB xdg-open https://www.pling.com/p/1465345/ > /dev/null 2>&1 || am start -a android.intent.action.VIEW -d https://www.pling.com/p/1465345/ > /dev/null 2>&1
-  echo "  DONE"
-  echo " "
-  echo "- FLASH DOWNLOADED FILE BEFORE REBOOTING!"
-  echo " "
-  echo "================================================"
-  echo "                            MAGNETAR © 2019-2022"
-  [ ! -z $MODPATH ] && $BB rm -rf $MODPATH
+  for i in /data/adb/*/*/module.prop; do
+    MDESC=$($BB grep -sm1 'description' $i | $BB cut -d'=' -f2)
+    if [[ "$MDESC" = *"Device Performance Optimizer"* ]]; then
+      MNAME=$($BB grep -sm1 'name=' $i | $BB cut -d'=' -f2)
+      if [[ "$MNAME" = "MAGNETAR+" ]]; then
+        resetprop magnetar.update initialize
+        $BB sleep 1
+        magnetar --update ota
+      else
+        echo "- DOWNLOAD LATEST MAGNETAR RELEASE"
+        $BB sleep 3
+        echo " "
+        echo "- OPENING PLING..."
+        [ -n "$(which xdg-open)" ] && xdg-open https://www.pling.com/p/1465345/ > /dev/null 2>&1 || am start -a android.intent.action.VIEW -d https://www.pling.com/p/1465345/ > /dev/null 2>&1
+        echo "  DONE"
+        echo " "
+        echo "- FLASH DOWNLOADED FILE BEFORE REBOOTING!"
+        echo " "
+        echo "======================================================"
+        echo "                                  MAGNETAR © 2019-2022"
+      fi
+      break
+    fi
+  done
+  [ -n "$MODPATH" ] && $BB rm -rf $MODPATH
   $BB rm -rf $TMPDIR
   exit 0
 }
