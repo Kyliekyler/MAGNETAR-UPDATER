81f57b7141fb195436c56f914c100909ffc93712
Kyliekyler <siosonkylebrandon@gmail.com>
2023-02-21 00:20:31 +0800

    updater: update to v4
    Signed-off-by: Kyliekyler <siosonkylebrandon@gmail.com>

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 print_modname() {
-  $BB sleep 1
+  sleep 1
   echo " "
-  echo "======================================================"
-  echo "⠀⠀⠀⠀⠀⢀⣠⣤⡶⣢⣶⣶⣦⡀"
-  echo "⠀⠀⠀⢀⣴⡿⠋⠁⠼⢿⡿⠿⣿⣿⣼⣦⡀⠀⠀ ⠀M A G N E T A R"
-  echo "⠀⠀⠀⣾⠏⠀⢀⣴⣿⣿⣿⣿⣮⡿⠁⠹⣷⡀  ⠀----------------"
-  echo "⠀⠀⠸⣫⣶⣇⣿⣿⣿⣿⣿⣿⡿⠿⠀⠀⣿⡇   DEV: Kyliekyler"
-  echo "⠀⠀⢸⣿⣿⡏⣿⣿⣿⣿⢏⣷⣿⣿⣷⣆⢿⡇"
-  echo "⠀⠀⠈⠿⣿⣿⣮⡿⣿⣟⢾⣿⣿⣿⣿⣿⡞⠀⠀  ░▒░▒░▒░▒░▒░▒░"
-  echo "⠀⠀⠀⠀⠲⣿⣅⡀⠀⠈⠘⢿⣿⣿⣿⠟⠀⠀⠀  ▒░▒░▒░▒░▒░▒░▒"⠀
-  echo "⠀⠀⠀⠀⠀⠈⠙⠛⠿⠿⠿⠷⠒⠂"
-  echo "======================================================"
+  echo "=================================================="
+  echo "      ⢀⣠⣤⡶⣢⣶⣶⣦⡀"
+  echo "   ⢀⣴⡿⠋⠁⠼⢿⡿⠿⣿⣿⣼⣦⡀     M A G N E T A R"
+  echo "  ⢀⣾⠏ ⢀⣴⣿⣿⣿⣿⣮⡿⠁⠹⣷⡀   -----------------"
+  echo "  ⠸⣫⣶⣇⣿⣿⣿⣿⣿⣿⡿⠿⡀ ⣿⡇   DEV: Kyliekyler"
+  echo "  ⢸⣿⣿⡏⣿⣿⣿⣿⢏⣷⣿⣿⣷⣆⢿⡇"
+  echo "  ⠈⠿⣿⣿⣮⡿⣿⣟⢾⣿⣿⣿⣿⣿⡞    ░▒░▒░▒░▒░▒░▒░"
+  echo "    ⠲⣿⣅⡀  ⠈⠘⢿⣿⣿⣿⠟     ▒░▒░▒░▒░▒░▒░▒"
+  echo "      ⠈⠙⠛⠿⠿⠿⠷⠒⠂"
+  echo "=================================================="
 }
 
 on_install() {
   echo " "
-  for i in /data/adb/*/*/module.prop; do
-    MNAME=$(awk -F'=' '/name=/{print $2}' "$i")
-    MDESC=$(awk -F'=' '/description=/{print $2}' "$i")
+  find /data/adb -type f -name 'module.prop' | while IFS= read -r i; do
+    MNAME="$(awk -F'=' '/name=/{print $2}' $i)"
+    MDESC="$(awk -F'=' '/description=/{print $2}' $i)"
     if echo "$MNAME" | grep -q 'MAGNETAR' ||  echo "$MDESC" | grep -q 'Device Performance Optimizer'; then
       resetprop magnetar.update initialize
-      $BB sleep 1
+      sleep 1
       magnetar --update ota
       break
     fi
