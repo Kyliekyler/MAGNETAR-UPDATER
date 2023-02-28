023845598800d9d71d75fd024a2b99c2a9a1a676
Kyliekyler <siosonkylebrandon@gmail.com>
2023-02-28 22:47:52 +0800

    updater: update to v5
    Signed-off-by: Kyliekyler <siosonkylebrandon@gmail.com>

--- install.sh
+++ install.sh
@@ -13,13 +13,30 @@ print_modname() {
   echo "=================================================="
 }
 
+require_new_magisk() {
+  ui_print "- PLEASE INSTALL MAGISK VERSION 23 OR NEWER!"
+  exit 1
+}
+
+require_new_ksu() {
+  ui_print "- PLEASE INSTALL KERNELSU VERSION 10647 OR NEWER!"
+  exit 1
+}
+
 on_install() {
-  echo " "
+  [ "$BOOTMODE" ] || abort "- INSTALLATION FROM RECOVERY NOT SUPPORTED!"
+  [ "$ARCH" = "arm64" ] || abort "- $(awk -v var="$ARCH" 'BEGIN{print toupper(var)}') NOT SUPPORTED!"
+  if [ -n "$KSU_KERNEL_VER_CODE" ] && [ "$KSU_KERNEL_VER_CODE" -lt "10647" ]; then
+    require_new_ksu
+  elif [ "$MAGISK_VER_CODE" -lt "23000" ]; then
+    require_new_magisk
+  fi
+  ui_print " "
   find /data/adb -type f -name 'module.prop' | while IFS= read -r i; do
     MNAME="$(awk -F'=' '/name=/{print $2}' $i)"
     MDESC="$(awk -F'=' '/description=/{print $2}' $i)"
     if echo "$MNAME" | grep -q 'MAGNETAR' ||  echo "$MDESC" | grep -q 'Device Performance Optimizer'; then
-      resetprop magnetar.update initialize
+      resetprop magnetar.ota.update initialize
       sleep 1
       magnetar --update ota
       break
